fea2c0a189a48fe2367565899219bb7931e6c1e0
n-freman <smiron1703@gmail.com>
2024-04-02 08:50:36 +0500

    add template for email activation

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,9 +1,11 @@
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser
 from rest_framework.routers import Response
 from rest_framework.views import APIView
 
+from .otp import OTP
 from .serializers import RegistrationSerializer
 
 
@@ -18,9 +20,18 @@ class RegistrationAPIView(APIView):
         if serializer.is_valid():
             user, profile  = serializer.create(serializer.validated_data)
             # send otp to user.email
+            context = {
+                'otp': OTP.get_password(user.email),
+                'first_name': profile.first_name,
+                'last_name': profile.last_name
+            }
+            message = render_to_string(
+                'users/email_activation_message.html',
+                context 
+            )
             send_mail(
-                "Subject",
-                "Message",
+                "Email Activation",
+                message,
                 "nazar@gmail.com",
                 [user.email]
             )
@@ -30,3 +41,4 @@ class RegistrationAPIView(APIView):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
+

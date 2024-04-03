0bcb5beff251a151c7f756bf431e1d32cc0c9bd7
  Dovran Orazov <orazowd77@gmail.com>
2024-04-03 22:24:18 +0500

    sidebar ready

--- frontend/src/components/personal-area-layout/header/HeaderPersonalArea.tsx
+++ frontend/src/components/personal-area-layout/header/HeaderPersonalArea.tsx
@@ -1,8 +1,25 @@
+import IconUI from '@/components/ui/icon/Icon'
+import Image from 'next/image'
 import styles from './HeaderPersonalArea.module.scss'
 const HeaderPersonalArea = () => {
 	return (
-		<header className={styles.header}>Header</header>
-
+		<header className={styles.header}>
+			<div className={styles.row}>
+				<div className={styles.profile}>
+					<div className={styles.img}>
+						<Image src='/profile.svg' alt='person' fill />
+					</div>
+					<div className={styles.content}>
+						<h2>Emir Hudayberdiyew</h2>
+						<p>Онлайн</p>
+					</div>
+					<div className={styles.buttons}>
+						<IconUI icon='notification' />
+						<IconUI icon='threeDots' />
+					</div>
+				</div>
+			</div>
+		</header>
 	)
 }
 

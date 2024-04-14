fadfa4125a85fa14b5827447c8e848bd857edef3
n-freman <smiron1703@gmail.com>
2024-04-14 10:04:40 +0500

    attach all models to admin page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = 'Tech Recruiter'
+admin.site.index_title = 'Панель администрации'
+admin.site.site_title = 'Управление'
+
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path("admin/", admin.site.urls),

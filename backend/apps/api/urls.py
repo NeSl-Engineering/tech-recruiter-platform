6d330eb3d991ae657fbfaac6c2efe8e23ea9f111
n-freman <smiron1703@gmail.com>
2024-04-17 08:55:17 +0500

    add jwt verification endpoint

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView
+    TokenRefreshView,
+    TokenVerifyView
 )
 
 from users.views import (
@@ -27,6 +28,7 @@ urlpatterns = [
     path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('auth/register/', RegistrationAPIView.as_view(), name='register_user'),
     path('auth/resend-otp/', OTPResendAPIView.as_view(), name='resend-otp'),
     path('auth/verify-email/', EmailVerificationAPIView.as_view(), name='verify_email'),

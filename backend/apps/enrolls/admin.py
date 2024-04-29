1273b1b233d06ef5852412c34ec98306bf950609
n-freman <smiron1703@gmail.com>
2024-04-29 10:18:43 +0500

    add enrolls to admin page

--- backend/apps/enrolls/admin.py
+++ backend/apps/enrolls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Enroll
+
+
+class EnrollAdminConfig(admin.ModelAdmin):
+    list_display = ['course', 'user', 'created_at']
+
+
+admin.site.register(Enroll, EnrollAdminConfig)
+
--- backend/apps/enrolls/apps.py
+++ backend/apps/enrolls/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class EnrollsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "enrolls"
+    verbose_name = 'Записи на курсы'
+

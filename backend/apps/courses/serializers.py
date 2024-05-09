3483817d2ea7d9e8095f862d9dbc58833098aad0
n-freman <smiron1703@gmail.com>
2024-05-09 10:50:21 +0500

    update course categories serializer

--- backend/apps/courses/serializers.py
+++ backend/apps/courses/serializers.py
@@ -32,14 +32,22 @@ class ModuleSerializer(serializers.ModelSerializer):
         exclude = ('course',)
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Category
+        model = Course
         fields = '__all__'
 
 
-class CourseSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+    courses = CourseSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'courses']
+
+
+class CourseWithModulesSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True)
     
     class Meta:

edce5a47dcd002e4a9442c4320d04633a91664b3
n-freman <smiron1703@gmail.com>
2024-05-15 09:16:56 +0500

    update courses & models admin pages

--- backend/apps/courses/admin.py
+++ backend/apps/courses/admin.py
@@ -7,6 +7,7 @@ from .models import Category, Course, LessonMaterial, LessonVideo, Module
 class ModuleAdminInline(admin.TabularInline):
     model = Module
     extra = 0
+    ordering = ['ordinal_number']
 
 
 class CourseAdminConfig(admin.ModelAdmin):
@@ -19,9 +20,31 @@ class CourseAdminConfig(admin.ModelAdmin):
         ''')
 
 
+class LessonVideoAdminInline(admin.TabularInline):
+    model = LessonVideo
+    extra = 0
+    ordering = ['ordinal_number']
+
+
+class LessonMaterialInline(admin.TabularInline):
+    model = LessonMaterial
+    extra = 0
+    ordering = ['ordinal_number']
+
+
+class ModuleAdminConfig(admin.ModelAdmin):
+    list_display = ['title', 'course', 'ordinal_number']
+    inlines = [LessonVideoAdminInline, LessonMaterialInline]
+
+
+class LessonDataAdminConfig(admin.ModelAdmin):
+    list_display = ['module', 'ordinal_number']
+    ordering = ['module', '-ordinal_number']
+
+
 admin.site.register(Category)
 admin.site.register(Course, CourseAdminConfig)
-admin.site.register(Module)
-admin.site.register(LessonVideo)
-admin.site.register(LessonMaterial)
+admin.site.register(Module, ModuleAdminConfig)
+admin.site.register(LessonVideo, LessonDataAdminConfig)
+admin.site.register(LessonMaterial, LessonDataAdminConfig)
 

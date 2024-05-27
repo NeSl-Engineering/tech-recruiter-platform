a1c847a228ac43fd9b19839cdc475ba9673fdbf7
  Dovran Orazov <orazowd77@gmail.com>
2024-05-27 17:20:19 +0500

    styles in blog page edited

--- frontend/src/app/blog/Blog.tsx
+++ frontend/src/app/blog/Blog.tsx
@@ -1,5 +1,7 @@
 import BlogItems from '@/components/main/blog-items/BlogItems'
 import ButtonGroup from '@/components/main/button-group/ButtonGroup'
+import Contacts from '@/components/main/contacts/Contacts'
+import SubscribeLink from '@/components/main/subscribe-link/SubscribeLink'
 import TitleSearch from '@/components/main/title-search/TitleSearch'
 import styles from './Blog.module.scss'
 
@@ -9,6 +11,8 @@ const Blog = () => {
 			<TitleSearch />
 			<ButtonGroup />
 			<BlogItems />
+			<SubscribeLink />
+			<Contacts />
 		</div>
 	)
 }

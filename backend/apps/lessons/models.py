49bf7dc974fe31e87546bb214a7663b333a2e66b
n-freman <smiron1703@gmail.com>
2024-05-31 15:05:21 +0500

    add slug field to lesson model

--- backend/apps/lessons/models.py
+++ backend/apps/lessons/models.py
@@ -1,4 +1,6 @@
+from autoslug import AutoSlugField
 from django.db import models
+from slugify import slugify
 
 from courses.models import Module
 
@@ -13,6 +15,13 @@ class Lesson(models.Model):
     title = models.TextField(max_length=120, null=True)
     video = models.FileField(upload_to='lessons', verbose_name='Видео')
     ordinal_number = models.PositiveIntegerField(verbose_name='Порядковый номер')
+    slug = AutoSlugField(
+        populate_from='title',
+        slugify=slugify,
+        null=True,
+        blank=True,
+        default=None
+    )
 
     class Meta:
         db_table = 'lessons'

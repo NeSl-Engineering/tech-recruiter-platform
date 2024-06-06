4c03db0a261c05b2bacfb11fa03a4688c826e675
n-freman <smiron1703@gmail.com>
2024-06-06 12:49:49 +0500

    update email backend

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -181,7 +181,7 @@ MEDIA_URL =  '/media/'
 MEDIA_ROOT = 'mediafiles'
 
 # Email message configs
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')

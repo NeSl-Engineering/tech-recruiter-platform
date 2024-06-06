6e88cb5a77e015f033417c1d52030642a713377c
n-freman <smiron1703@gmail.com>
2024-06-06 13:00:18 +0500

    update email backend configs

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -186,6 +186,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
 EMAIL_TIEMOUT = os.getenv('EMAIL_TIEMOUT')
 
 OTP_EXPIRE_TIME = 60 * 2

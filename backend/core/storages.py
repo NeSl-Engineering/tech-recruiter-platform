1fbf04a6b76cfcfd4b0059e62b2aab40eca0a551
n-freman <smiron1703@gmail.com>
2024-06-06 12:38:35 +0500

    update storages url

--- backend/core/storages.py
+++ backend/core/storages.py
@@ -5,6 +5,6 @@ class CustomStorage(S3StaticStorage):
 
     def url(self, name):
         url = super().url(name)
-        url = f'http://localhost:9000/tech-recruiter{url.split("tech-recruiter")[-1]}'
+        url = f'http://158.160.89.94:9000/tech-recruiter{url.split("tech-recruiter")[-1]}'
         return url
 

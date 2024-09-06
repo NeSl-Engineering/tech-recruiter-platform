8cf1766d68fb6ddc221e19faf608154bf6016e3e
Dovran Orazov <orazowd77@gmail.com>
2024-09-06 15:16:59 +0500

    fixed bug

--- frontend/src/services/courses.service.ts
+++ frontend/src/services/courses.service.ts
@@ -25,10 +25,7 @@ class CourseService {
 		)
 		return response?.data
 	}
-<<<<<<< HEAD
 	
-=======
->>>>>>> 21deae507e16294134c3821cc23474e3e0b6ffa7
 }
 
 export const courseService = new CourseService()
